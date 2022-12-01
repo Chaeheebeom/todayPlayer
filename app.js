98a5a4aa98012c6c0a66d36378a3448874b0d274
ChaeHeeBEom <zzcjs123@gmail.com>
2022-12-01 22:28:01 +0900

    no message

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const express = require("express");
 const app = express();
 
 const mainRouter = require("./router/mainRouter");
-const jsRouter = require("./router/jsRouter");
-
 app.set("view engine", "ejs");
 
 console.log("__dirname", __dirname);
@@ -12,8 +10,6 @@ console.log("__dirname", __dirname);
 app.use(express.static(__dirname + '/public'));
 
 app.use("/todayPlayer", mainRouter);
-
-app.use("/js", jsRouter);
 
 const port = 3000;
 
